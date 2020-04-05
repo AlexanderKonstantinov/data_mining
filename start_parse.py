e5136881f96062a2ef1deb1cf0f4d67ae4467bfd
Александр <alex-sdt@yandex.ru>
2020-04-05 14:05:36 +1000

    task is complete

--- start_parse.py
+++ start_parse.py
@@ -1,9 +1,16 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from blogparse import settings
 from blogparse.spiders.habr_blog import HabrBlogSpider
 from blogparse.spiders.avito import AvitoSpider
+from blogparse.spiders.instagram import InstagramSpider
 
+env_path = Path(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=env_path)
 
 if __name__ == '__main__':
     craw_settings = Settings()
@@ -15,5 +22,6 @@ if __name__ == '__main__':
     craw_settings.setmodule(settings)
     crawler_proc = CrawlerProcess(settings=craw_settings)
     # crawler_proc.crawl(HabrBlogSpider)
-    crawler_proc.crawl(AvitoSpider)
+    # crawler_proc.crawl(AvitoSpider)
+    crawler_proc.crawl(InstagramSpider, logpass=(os.getenv('LOGIN'), os.getenv('PWD')))
     crawler_proc.start()
